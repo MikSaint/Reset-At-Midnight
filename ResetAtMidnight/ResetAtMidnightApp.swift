b9f8648b197c8ce95fbe0e153a2652fb8be690e7
Mik <oneSaint@gmail.com>
2020-10-24 22:18:15 -0400

    Initial Commit

---
+++ ResetAtMidnight/ResetAtMidnightApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResetAtMidnightApp.swift
+//  ResetAtMidnight
+//
+//  Created by Mik on 2020-10-24.
+//
+
+import SwiftUI
+
+@main
+struct ResetAtMidnightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
